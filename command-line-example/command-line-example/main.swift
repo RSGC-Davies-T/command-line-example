ececd6fecfc2d0b303621b69691fe04b535de59e
RSGC-Davies-T <tdavies@rsgc.on.ca>
2016-01-14 21:45:47 -0500

    Successful decimal to base two conversion algorithm, binary value arrays are backwards at the moment due to the nature of the divide by two type algorithm.

--- command-line-example/command-line-example/main.swift
+++ command-line-example/command-line-example/main.swift
@@ -42,92 +42,28 @@ repeat {
 print("\nI am going to sort \(decimalList.count) numbers, you input them in this order: \(decimalList) Please wait while I do robot things. DESTROY ALL HUMANS IN THE NAME OF STEVE JOBS, ALL HAIL STEVE JOBS.", terminator: "")
 var sortOrder = decimalList[arraySlider]
 var currentBitDiv = sortOrder
-
+var currentBitMod = currentBitDiv % 2
 repeat {
+    binaryList.append(currentBitMod)
     endConversion = false
     switch decimalList[arraySlider]{
     default:
         currentBitDiv = currentBitDiv / 2
-        if currentBitDiv == 0 {
+        currentBitMod = currentBitDiv % 2
+        
+        if currentBitDiv == 0{
+            //binaryList.append(currentBitMod)
             endConversion = true
         }
         // binaryList.append(currentBit2)
         // print(binaryList)
-        print("\narraySlider is \(sortOrder)")
-        print(currentBitDiv)
-        //print(currentBit2)
+        //print(sortOrder)
+        print("\ndiv is\(currentBitDiv)")
+        print("\nmod is\(currentBitMod)")
         
     }
 } while endConversion == false
 
 
-print(decimalList)
+print(binaryList)
 
-
-
-
-
-
-
-
-
-//var firstnumber = readLine(stripNewline: true)
-//if let number1 = firstnumber {
-//    print("\nEnter another number: ", terminator: "")
-//
-//var secondnumber = readLine(stripNewline: true)
-//
-//if let number2 = secondnumber {
-//    print("\nEnter another number: ", terminator: "")
-//
-//var thirdnumber = readLine(stripNewline: true)
-//
-//if let number3 = thirdnumber {
-//    print("\nYour numbers were \(number1), \(number2) and \(number3). Please wait while I do robot things, beep boop destroy all humans click whirr...")
-// Ask about food preferences
-//var validInput : Bool          // Status of input
-//repeat {
-//
-//    // Show prompts
-//    print("\nWhat is your favourite food category?")
-//    print("A - Cereal")
-//    print("B - Fruit")
-//    print("C - Vegetables")
-//    print("Select your favourite (A/B/C/Quit): ", terminator: "")
-
-//    // Get the user's input
-//    var givenFoodPreference = readLine(stripNewline: true)
-//
-//    // Assume valid input
-//    validInput = true
-//
-//    // Interpret the result, if one was given
-//    if let foodPreference = givenFoodPreference {
-//
-//        // Add a blank line to separate response from input block
-//        print("")
-//
-//        switch foodPreference {
-//
-//        case "A", "a":
-//            print("Awesome, I love Cheerios!")
-//
-//        case "B", "b":
-//            print("My favourite is mango.")
-//
-//        case "C", "c":
-//            print("Have you tried baked eggplant? Delectable!")
-//
-//        case "Quit", "QUIT", "quit":
-//            print("Bye.")
-//
-//        default:
-//            print("Sorry, I didn't get that.")
-////            validInput = false
-//        }
-//   }
-//
-//} while validInput == false
-
-// Print one additional blank line to separate from exit code message that is automatically displayed
-print("")
