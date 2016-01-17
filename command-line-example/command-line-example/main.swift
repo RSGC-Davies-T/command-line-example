44458fd767853e4e9ebbcae880344911ce4021d8
RSGC-Davies-T <tdavies@rsgc.on.ca>
2016-01-17 13:05:41 -0500

    algorithm implemented that sorts all of the numbers into high or low groups based on their first binary digit

--- command-line-example/command-line-example/main.swift
+++ command-line-example/command-line-example/main.swift
@@ -11,6 +11,9 @@ var masterCancel : Bool
 var endLoop : Bool
 var userInput : String = readLine()!
 var arraySlider : Int = 0
+var sortSlider = 0
+var endSort : Bool
+var endMSort : Bool
 repeat {
     
     endLoop = false
@@ -48,51 +51,87 @@ var currentBitOrder = binaryList.count
 binarySort.removeAtIndex(0)
 
 repeat{
-masterCancel = true
-var sortOrder = decimalList[arraySlider]
-var currentBitDiv = sortOrder
-var currentBitMod = currentBitDiv % 2
-var currentBitOrder = binaryList.count
-
-repeat {
-    binaryList.append(currentBitMod)
-    endConversion = false
-    switch decimalList[arraySlider]{
-    default:
-        currentBitDiv = currentBitDiv / 2
-        currentBitMod = currentBitDiv % 2
-        
-        if currentBitDiv == 0{
-           endConversion = true
-            binaryList = binaryList.reverse()
-            binarySort.append(binaryList)
-           // print("\n\(binaryList)")
-            arraySlider = arraySlider + 1
-            for (index, element) in binaryList.enumerate() {
-                binaryList.removeAtIndex(0)
-                
-            }
-            //binarySort.append(binaryList)
-        }
-        if numbersToSort == arraySlider{
+    masterCancel = true
+    var sortOrder = decimalList[arraySlider]
+    var currentBitDiv = sortOrder
+    var currentBitMod = currentBitDiv % 2
+    var currentBitOrder = binaryList.count
+    
+    repeat {
+        binaryList.append(currentBitMod)
+        endConversion = false
+        switch decimalList[arraySlider]{
+        default:
+            currentBitDiv = currentBitDiv / 2
+            currentBitMod = currentBitDiv % 2
             
-           endConversion = true
-            masterCancel = false
+            if currentBitDiv == 0{
+                endConversion = true
+                binaryList = binaryList.reverse()
+                binarySort.append(binaryList)
+                // print("\n\(binaryList)")
+                arraySlider = arraySlider + 1
+                for (index, element) in binaryList.enumerate() {
+                    binaryList.removeAtIndex(0)
+                    
+                }
+                //binarySort.append(binaryList)
+            }
+            if numbersToSort == arraySlider{
+                    
+                    endConversion = true
+                    masterCancel = false
+            }
+            
+            //print("\narrays is \(arraySlider)")
+            //print("\nnumbers is \(numbersToSort)")
+            //print("\ndiv is \(currentBitDiv)")
+            //print("\nmod is \(currentBitMod)")
+            
         }
-        
-        //print("\narrays is \(arraySlider)")
-        //print("\nnumbers is \(numbersToSort)")
-        //print("\ndiv is \(currentBitDiv)")
-        //print("\nmod is \(currentBitMod)")
-        
-    }
-} while endConversion == false
+    } while endConversion == false
 } while masterCancel == true
 
 //print(binaryList)
 //print("\nThere are: \(currentBitOrder) bits in this sequence, they are worth \(decimalList[arraySlider]) ", terminator: "")
 print("")
 print("\nIn binary these numbers are: \(binarySort)")
-//repeat {
-//
-//}
+var sortList = binarySort.count
+
+var indivList = 0
+var highNumbers = [[0,1]]
+var lowNumbers = [[0,1]]
+highNumbers.removeAtIndex(0)
+lowNumbers.removeAtIndex(0)
+var numberChecker = 0
+repeat {
+    endMSort = false
+    indivList = binarySort[sortSlider].count
+    numberChecker = binarySort[sortSlider][indivList-1]
+    repeat {
+        endSort = false
+        switch binarySort {
+        
+    default:
+            if numberChecker == 1 {
+            highNumbers.append(binarySort[sortSlider])
+            endSort = true
+            sortSlider = sortSlider + 1
+            print("\nHigh is: \(highNumbers)")
+            
+        }
+        if numberChecker == 0 {
+                lowNumbers.append(binarySort[sortSlider])
+                endSort = true
+                sortSlider = sortSlider + 1
+                print("\nLow is: \(lowNumbers)")
+        }
+        if sortList == sortSlider {
+                    endMSort = true
+        }
+        //master sorters
+        }
+    } while endSort == false
+} while endMSort == false
+print("\nFinal high is: \(highNumbers)")
+print("\nFinal low is: \(lowNumbers)")
